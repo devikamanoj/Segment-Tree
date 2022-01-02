0b50a4525758e5e511aab5c49eaeeb7b499d8ac4
Devika Manoj <dmenon138@gmail.com>
2022-01-02 15:11:04 +0530

    outline modified

--- Segment Tree/src/SegmentTree.java
+++ Segment Tree/src/SegmentTree.java
@@ -12,7 +12,8 @@ public class SegmentTree
         System.out.println("\n 1.Minimum Range Query");
         System.out.println(" 2.Maximum Range Query");
         System.out.println(" 3.Persistant Segment Tree");
-        System.out.println(" 4.Lazy Propogation\n");
+        System.out.println(" 4.Get sum of the given range");
+        System.out.println(" 5.Update the tree and get sum\n");
         
         int choice;
         char bool='y';
@@ -24,30 +25,44 @@ public class SegmentTree
             switch(choice)
             {
                 case 1:
+                {
                     System.out.print("\n -------------------");
                     System.out.print("\n Minimum Range Query");
                     System.out.print("\n -------------------\n");
                     MinQueryST.mainMin();
                     break;
-                
+                }
                 case 2:
+                {
                     System.out.print("\n -------------------");
                     System.out.print("\n Maximum Range Query");
                     System.out.print("\n -------------------\n");
                     MaxQueryST.mainMax();
                     break;
-                
+                }
                 case 3:
+                {
                     System.out.print("\n -------------------");
                     System.out.print("\n Persistent Segment Tree");
                     System.out.print("\n -------------------\n");
                     break;
-                
+                }
                 case 4:
+                {
                     System.out.print("\n -------------------");
-                    System.out.print("\n Lazy Propagation");
+                    System.out.print("\n Get sum of the given range");
                     System.out.print("\n -------------------\n");
+                    SumofGivenRange.Sum();
                     break;
+                }
+                case 5:
+                {
+                    System.out.print("\n -------------------");
+                    System.out.print("\n Update the tree and get sum");
+                    System.out.print("\n -------------------\n");
+                    UpdateGivenRange.update();
+                    break;  
+                }
             }
         
             System.out.print("Do you wish to continue? (Y/N) : ");
@@ -58,4 +73,5 @@ public class SegmentTree
        
         in.close();    
     }
+    
 }
--- Segment Tree/src/SumofGivenRange.java
+++ Segment Tree/src/SumofGivenRange.java
@@ -1,4 +1,82 @@
+import java.util.*;
 public class SumofGivenRange 
 {
+    static int [] ST=UpdateGivenRange.ST;
+    static int [] UP=UpdateGivenRange.UP;
+    static Scanner in = new Scanner(System.in);
     
+    static void Sum()
+    {
+        ArrayList <Integer> Range = new ArrayList<Integer>();
+        ArrayList <Integer> LeafNodes = new ArrayList<Integer>();
+        System.out.print(" Enter the number of Leaf Nodes: ");
+        int size=in.nextInt();
+
+        // taking the input of the leaf nodes
+        System.out.print(" Enter the Leaf Nodes: ");
+        LeafNodes=UpdateGivenRange.InputElements(LeafNodes, size);
+        UpdateGivenRange.constructST(LeafNodes, size);
+        System.out.print("\n Enter the range to compute the sum: ");
+        Range=UpdateGivenRange.InputElements(Range, 2);
+  
+        System.out.println("Sum of values in given range = " +getSum(size, Range.get(0), Range.get(1)));
+    }
+    // Return sum of elements in range from index qs (query start) to qe (query end).  It mainly uses getSumUtil()
+    static int getSum(int n, int qs, int qe)
+    {
+        // Check for erroneous input values
+        if (qs < 0 || qe > n - 1 || qs > qe)
+        {
+            System.out.println("Invalid Input");
+            return -1;
+        }
+      
+        return getSumUtil(0, n - 1, qs, qe, 0);
+    }
+          
+    /*  A recursive function to get the sum of values in given range of the array. The following are parameters for this function.
+        si --> Index of current node in the segment tree.
+            Initially 0 is passed as root is always at'
+            index 0
+        ss & se  --> Starting and ending indexes of the
+            segment represented by current node,
+            i.e., tree[si]
+        qs & qe  --> Starting and ending indexes of query
+            range */
+    static int getSumUtil(int ss, int se, int qs, int qe, int si)
+    {
+        // If lazy flag is set for current node of segment tree, then there are some pending updates. So we need to
+        // make sure that the pending updates are done before processing the sub sum query
+        if (UP[si] != 0)
+        {
+            // Make pending updates to this node. Note that this node represents sum of elements in arr[ss..se] and
+            // all these elements must be increased by lazy[si]
+            ST[si] += (se - ss + 1) * UP[si];
+              
+            // checking if it is not leaf node because if it is leaf node then we cannot go further
+            if (ss != se)
+            {
+                // Since we are not yet updating children os si, we need to set lazy values for the children
+                UP[si * 2 + 1] += UP[si];
+                UP[si * 2 + 2] += UP[si];
+            }
+            // unset the lazy value for current node as it has been updated
+            UP[si] = 0;
+        }
+        // Out of range
+        if (ss > se || ss > qe || se < qs)
+        {
+            return 0;
+        }
+        // At this point sure, pending lazy updates are done for current node. So we can return value (same as was for query in our previous post)
+        // If this segment lies in range
+        if (ss >= qs && se <= qe)
+        {
+            return ST[si];
+        }
+        // If a part of this segment overlaps with the given range
+        int mid = (ss + se) / 2;
+        
+        return getSumUtil(ss, mid, qs, qe, 2 * si + 1) + getSumUtil(mid + 1, se, qs, qe, 2 * si + 2);
+    }
 }
